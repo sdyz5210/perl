cdfdba55beadcafe7ec014d8d649c9a382387479
sdyz5210 <sdyz5210@gmail.com>
2017-08-17 09:51:17 +0800

    使用perl生成公钥私钥文件，java进行读取

--- files/Crypt_PK_RSA.pl
+++ files/Crypt_PK_RSA.pl
@@ -18,6 +18,17 @@ sub encryptString {
 	print $str_de;
 }
 
+=key
+	DER：JAVA默认使用的格式
+	PEM：PEM文件是Base64编码的证书。PEM是OpenSSL和许多其他SSL工具的标准格式，OpenSSL使用PEM文件格式存储证书和密钥。
+
+	注意：此处生成的pem及der文件，使用java语言没办法识别，暂时没有找到perl在这方便的配置和修改。原因是生成的pem没有转换成pkcs8格式
+
+	使用openssl生成pem或der可以，如下
+	openssl genrsa -out rsakey.priv.pem 1024
+	openssl pkcs8 -topk8 -inform PEM -outform DER -in rsakey.priv.pem -out rsakey.priv.der -nocrypt
+	openssl rsa -in rsakey.priv.pem -pubout -outform DER -out rsakey.pub.der
+=cut
 sub saveKey{
 	my $pk = Crypt::PK::RSA->new();
 	$pk->generate_key(128, 65537);
@@ -41,6 +52,17 @@ sub encryptStringImportKey {
 	print $str_de;
 }
 
+sub decryptStringFromJava{
+	my $pkrsa = Crypt::PK::RSA->new;
+	$pkrsa->import_key("rsakey.pub.der");
+	$pkrsa->import_key("rsakey.priv.der");
+
+	my $str_en = 'FIAJ8iCgbDU9sJUHrsAoUt9h+lG4GJ4geoBK8ePR4ovsrd9GhbP/cH8+ayscgaYlccnIIbPGA233vW9nRQ4iWVYwAd3jTfik8BvO1r3PFgYpWJn1jXNZ68B9UrrgiOyN8O1xyxM3j0rt8camFACbVQwc5twBWOcuqpaykm83jL0=';
+	my $str_de = $pkrsa->decrypt(decode_base64($str_en));
+	print $str_de;
+}
+
 #encryptString();
 saveKey();
 #encryptStringImportKey();
+#decryptStringFromJava();
